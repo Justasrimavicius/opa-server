da0d6cc13e7b7176b18cdd4ec91ed543c6ef14bd
Justinas <justasrimavicius@gmail.com>
2022-12-06 10:24:16 +0200

    x

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({ secret: "cats", resave: true, saveUninitialized: true}));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://opaa.netlify.app');
     // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-
+//
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     next();
